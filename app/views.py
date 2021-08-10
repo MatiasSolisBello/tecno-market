eba5e0f8e24971e31f155a19301fab05d4c2e1a0
MatiasSolisBello <matias_solis11@hotmail.com>
2021-08-10 12:43:14 -0400

    Creacion de Proyecto

---
+++ app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request, 'app/home.html')
+
+def contacto(request):
+    return render(request, 'app/contacto.html')
