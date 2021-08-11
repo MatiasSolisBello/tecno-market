14201881adf760e2f0ecd3d9f2603c0b9f0cc18f
MatiasSolisBello <matias_solis11@hotmail.com>
2021-08-10 21:59:32 -0400

    Modelos y SQLite

--- app/models.py
+++ app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Marca(models.Model):
+    nombre = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.nombre
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=200)
+    precio = models.IntegerField()
+    descripcion = models.TextField()
+    nuevo = models.BooleanField()
+    marca = models.ForeignKey(Marca, on_delete=models.PROTECT)
+    fecha_fabricacion = models.DateField()
+
+    def __str__(self):
+        return self.nombre
