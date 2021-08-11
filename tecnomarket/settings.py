6d1238638b31e64b62461ba6b129b830ba39d7fc
MatiasSolisBello <matias_solis11@hotmail.com>
2021-08-10 22:28:49 -0400

    Subir Imagenes

--- tecnomarket/settings.py
+++ tecnomarket/settings.py
@@ -120,6 +120,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+# Media files
+import os
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
