b5561392eeb10c76b0f5b9f47fc35279bf8312b8
MatiasSolisBello <matias_solis11@hotmail.com>
2021-08-12 11:32:53 -0400

    Crispy Forms

--- tecnomarket/settings.py
+++ tecnomarket/settings.py
@@ -39,7 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'app',
     'django.contrib.humanize',
+    'crispy_forms',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
